3137aaaaf1ef95763d36ebf922603172541d3a60
Ana Luisa <125583157+AnaLuisaFav@users.noreply.github.com>
2023-05-30 15:07:27 -0300

    Add files via upload

---
+++ init.js
@@ -0,0 +1,9 @@
+var botaoMenu = document.querySelector('.botao-menu');
+var menu = document.querySelector('.menu');
+
+botaoMenu.addEventListener('click', function() {
+  menu.classList.toggle('aberto');
+})
+
+
+
